cf02d142e84a8a9b8ea3dc35040c9ee56df8a512
Simon Strom <simon.strom@gmail.com>
2012-11-12 17:48:00 +0100

    skeletons form Branch and bound and cutting plane

---
+++ src/CuttingPlane.java
@@ -0,0 +1,16 @@
+
+public class CuttingPlane implements Algorithm {
+	// http://www.tsp.gatech.edu/methods/dfj/index.html
+	// http://en.wikipedia.org/wiki/Cutting-plane_method
+	World world;
+	
+	public CuttingPlane(World w) {
+		world = w;
+	}
+	@Override
+	public int[] solve() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
