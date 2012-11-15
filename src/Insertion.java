834e134c5025339d8a73a6016794de6ba8597d7c
Simon Strom <simon.strom@gmail.com>
2012-11-15 21:49:14 +0100

    working insertion

--- src/Insertion.java
+++ src/Insertion.java
@@ -29,21 +29,29 @@ public class Insertion extends Algorithm{
 		
 		while (true) {
 			int addAfter = -1;
+			int addBefore = -1;
 			int bestCity = -1; 
+			
 			for (int i = 0; i < list.size(); i++) {
 				
+				int nextCity = addAfter + 1;
+				if (nextCity > list.size()) {
+					nextCity = 0;
+				}
 				int tmp_new_city = world.getNearestCity(list.get(i), visited);
 				if (addAfter < 0) {
 					addAfter = i;
 					bestCity = tmp_new_city;
-				} else if (world.getDistanceTo(addAfter, bestCity) > world.getDistanceTo(list.get(i), tmp_new_city)) {
+					addBefore = nextCity;
+				} else if ( distance(addAfter, bestCity, nextCity) > distance(i, tmp_new_city, nextCity)) {
+//					System.out.println("add " + bestCity + " after " + addAfter + " total \tlist now:\t " + list.size() + "distance before: " +  world.getDistanceTo(addAfter, bestCity) + world.getDistanceTo(bestCity, nextCity) + " distance after: " + world.getDistanceTo(list.get(i), tmp_new_city)  + world.getDistanceTo(tmp_new_city, list.get(nextCity)));
 					bestCity = tmp_new_city;
 					addAfter = i;
+					addBefore = nextCity;
 				}
-				
 			}
-			System.out.println("add " + bestCity + " after " + addAfter + " total \tlist now:\t " + list.size());
 			
+//			System.out.println("selecting " + bestCity + "after " + addAfter + " distance: " + (world.getDistanceTo(addAfter, bestCity) + world.getDistanceTo(bestCity, addBefore)) );
 			list.add(addAfter, bestCity);
 			visited[bestCity] = true;
 			
@@ -52,6 +60,12 @@ public class Insertion extends Algorithm{
 		}
 		
 		return getKattisOutput();
+	}
+	
+	private double distance(int c1, int c2, int c3) {
+		double dist1 = world.getDistanceTo(list.get(c1), c2);
+		double dist2 = world.getDistanceTo(c2, list.get(c3));
+		return dist1 + dist2;
 	}
 	
 	public int[] getKattisOutput() {
